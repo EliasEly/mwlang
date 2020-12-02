b6d164b5ae21c7ec4617bd89fec6b5c57e962587
Elias <elias.el@outlook.fr>
2020-12-02 15:09:42 +0100

    feat: AST type defined

--- ocaml/while.ml
+++ ocaml/while.ml
@@ -66,11 +66,6 @@ https://ltpf.gricad-pages.univ-grenoble-alpes.fr/PF/analist.ml
 exception Erreur
 type mwhile = char list -> char list
 
-type var = A | B | C | D ;;
-type const = Un | Zero ;;
-type exp = V of var | C of const ;;
-type instr = V of var | E of exp | W of exp | Vide ;;
-
 let list_of_string s =
   let rec boucle s i n =
     if i = n then [] else s.[i] :: boucle s (i+1) n
@@ -82,9 +77,11 @@ let terminal c : mwhile = fun l ->
   | x::l when x = c -> l
   | _ -> raise Erreur
 
-let (+>) (a: mwhile) (b : mwhile) : mwhile =
+(* a suivi de b *)  
+let (+>) (a: mwhile) (b: mwhile) : mwhile =
   fun l -> b (a l)
 
+(* a ou b *)
 let (+|) (a: mwhile) (b : mwhile) : mwhile =
   fun l -> try a l with Erreur -> b l
 
@@ -98,11 +95,12 @@ let term_b : mwhile = terminal 'b';;
 let term_c : mwhile = terminal 'c';;
 let term_d : mwhile = terminal 'd';;
 
-
 (**Terminal pour les variables allant de 0 à 1 *)
 let term_0 : mwhile = terminal '0';;
 let term_1 : mwhile = terminal '1';;
 
+(**Terminal pour la négation d'une variable *)
+let term_neg : mwhile = terminal '#';;
 
 (**Terminal pour le while et le if *)
 let term_w : mwhile = terminal 'w';;
@@ -149,7 +147,7 @@ let p_Affectation : mwhile =
     +>
       (p_Blanc)
     +>
-      (term_0 +| term_1 +| term_a +| term_b +| term_c +| term_d)
+      (term_0 +| term_1 +| term_a +| term_b +| term_c +| term_d +| term_neg)
     +>
       (p_Blanc)  
   ) l;;
@@ -187,13 +185,150 @@ let res = p_S t4;;
 let isWhileLanguage = fun l -> let l = p_S l in
   match l with
   | [] -> true
-  | _ -> false
-;;
+  | _ -> false;;
 
 isWhileLanguage t1;;
 isWhileLanguage t2;;
 isWhileLanguage t3;;
 isWhileLanguage t4;;
+
+(* Début de l'analyse syntaxique avec AST *)
+      
+type var = A | B | C | D;;
+type const = Un | Zero | Opposite;;
+type exp = V of var | Cst of const;;
+(*type instr = V of var | E of exp | W of exp;;*)
+
+type winstr = Vide | While of __while | If of __if | Assign of assign | Seq of sequence
+and __while = exp * winstr
+and __if = exp * winstr * winstr
+and sequence = winstr * winstr
+and assign = var * exp;;
+
+type state = int list;;
+
+type config =
+  | Inter of winstr * state
+  | Final of state;;
+
+
+(* Type pour fonctions qui épluchent une list de terminaux *)
+type 'term analist = 'term list -> 'term list;;
+
+(* Type pour fonctions qui épluchent une list de terminaux et retournent un résultat *)
+type ('r, 'term) ranalist = 'term list -> 'r * 'term list;;
+
+type ('x, 't) st = 't list -> 'x
+
+let terminal (c : 'a) : 'a analist = fun l -> 
+  match l with
+  | x::l when x = c -> l
+  | _ -> raise Erreur;;
+
+let (+>) (a: 't analist) (b: ('x, 't) st) : ( 'x, 't) st = 
+  fun l -> let l = a l in b l;;
+
+let (++>) (a: ('r, 't) ranalist) (b: 'r ->('x, 't) st) : ('x, 't) st = 
+  fun l -> let (x, l) = a l in b x l;;
+
+let (+|) (a: ('x, 't) st) (b: ('x, 't) st) : ('x, 't) st =
+  fun l -> try a l with Erreur -> b l;;
+  
+let return : 'r -> ('r, 't) ranalist =
+  fun x l -> (x, l);;
+
+  
+(**Terminal fin de ligne ; *)
+let term_eol : char analist = terminal ';';;
+
+(**Terminal pour les variables allant de a à d *)
+let term_a : (var, char) ranalist = terminal 'a' +> return A;;
+let term_b : (var, char) ranalist = terminal 'b' +> return B;;
+let term_c : (var, char) ranalist = terminal 'c' +> return C;;
+let term_d : (var, char) ranalist = terminal 'd' +> return D;;
+
+(**Terminal pour les variables allant de 0 à 1 *)
+let term_0 : (const, char) ranalist = terminal '0' +> return Zero;;
+let term_1 : (const, char) ranalist  = terminal '1' +> return Un;;
+
+(**Terminal pour la négation d'une variable *)
+let term_neg : (const, char) ranalist  = terminal '#' +> return Opposite;;
+
+(** Terminal pour le while et le if *)
+(** À faire au fil de l'eau pour pouvoir remplir le constructeur, voir p_S 
+  let term_w : (winstr, char) ranalist  = terminal 'w' +> return While;;
+  let term_if : (winstr, char) ranalist  = terminal 'i' +> return If;;
+*)
+
+(**Terminal pour les paranthèses et les acccolades *)
+let term_pg : char analist = terminal '(';;
+let term_pd : char analist = terminal ')';;
+let term_ag : char analist = terminal '{';;
+let term_ad : char analist = terminal '}';;
+
+let epsilon =  fun l -> l;;
+
+let rec p_Blanc : char analist = 
+  fun l -> (
+      ((terminal ' ') +| (terminal '\n') +| (terminal '\t') +> (p_Blanc))
+      +|
+      (epsilon)
+  ) l;;
+
+let p_Var : (var, char) ranalist = 
+  fun l -> (
+    (term_a +| term_b +| term_c +| term_d)
+  ) l;;
+
+let p_Cst : (const, char) ranalist = 
+  fun l -> (
+    (term_0 +| term_1)
+  ) l;;
+
+let p_Expression : (exp, char) ranalist =
+  fun l -> (
+    (p_Var ++> fun v -> return (V v)) +| (p_Cst ++> fun c -> return (Cst c))
+  ) l;;
+
+(* Le type (assign, char) ne marche pas winstr est obligatoire pouruqoi ?*)
+let p_Affectation : (winstr, char) ranalist =
+  fun l -> (
+      (p_Blanc)
+    +>
+      (p_Var) ++> fun var -> p_Blanc
+    +>
+      (terminal ':')
+    +>
+      (p_Blanc)
+    +> 
+      (terminal '=')
+    +>
+      (p_Blanc)
+    +>
+      (p_Expression) ++> fun value -> p_Blanc
+    +> return (Assign (var, value))
+  ) l;;
+
+
+
+let rec p_S : (winstr, char) ranalist = 
+  let p_L l = ((term_eol +> p_Blanc +> p_S) +| epsilon) l
+  and p_I l =  (p_Affectation 
+                +| (term_w +> p_Blanc
+                  +> term_pg +> p_Blanc +> p_Expression +> p_Blanc +> term_pd +> p_Blanc
+                  +> term_ag +> p_Blanc +> p_S +> p_Blanc +> term_ad) 
+                +| (term_if +> p_Blanc
+                  +> 
+                  term_pg +> p_Blanc +> p_Expression +> p_Blanc +> term_pd +> p_Blanc 
+                  +> 
+                  term_ag +> p_Blanc +> p_S +> term_ad +> p_Blanc 
+                  +> 
+                  term_ag +> p_Blanc +> p_S +> term_ad) 
+                +| epsilon) l
+  in fun l -> ((p_I +> p_L)+| epsilon) l
+
+
+(* Fin de l'analyse syntaxique avec AST *) 
 
 let init n  =
   let rec aux n s =
@@ -205,7 +340,7 @@ let init n  =
 let rec get x s = 
   match x with
   | 0 -> (match s with
-	        | [] -> 0
+          | [] -> 0
           | v::_ -> v)
   | x -> (match s with
           | [] -> 0
@@ -219,4 +354,4 @@ let rec update s v n =
   | v ->
     (match s with
       | [] -> 0::(update [] (v-1) n)
-      | a::l -> a::(update l (v-1) n));;
+      | a::l -> a::(update l (v-1) n));;    
