b42e06bb14f3a2c1092a4c19415f00749cfa94f2
Elias <elias.el@outlook.fr>
2020-12-04 17:08:07 +0100

    feat: SOS_Pcarre starting proof

--- ocaml/src/main.ml
+++ ocaml/src/main.ml
@@ -15,20 +15,21 @@ Les règles de transition de If sont donc :
 let t2 = list_of_string "a:=1;b:=1;w(a){b:=1}";;
 let _ = p_S t2;;
 
-let t3 = list_of_string "  a:=1;b:=1;i(1){b:=1}{c:=0}";;
+let t3 = list_of_string " a:=1;b:=1;i(1){b:=1}{c:=0}";;
 let _ = p_S t3;;
 
-let t4 = list_of_string "  a:=1;b:=1;a:=#;i(1){b:=1}{c:=0}";;
+let t4 = list_of_string "a:=1;b:=1;a:=#;i(1){b:=1}{c:=0}";;
 let _ = p_S t4;;
 
-let t3 = list_of_string
-"a:=1;
+let t5 = list_of_string
+"
+a:=1;
 c:=1;
 i(0)
   {b:=1}
   {c:=0}";; 
                           
-let instr = let (i, _) = p_S t3 in i;; 
+let instr = let (i, _) = p_S t5 in i;; 
 
 let _ = faire_un_pas instr (init 4);;
 let _ = executer instr (init 4);;
